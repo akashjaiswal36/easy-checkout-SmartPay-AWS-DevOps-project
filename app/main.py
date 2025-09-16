2551cab46f6e9062fc2814152105981a6cdaae9e
akashjaiswal36 <akash.jaiswal36@gmail.com>
2025-09-17 00:30:10 +0530

    added html

--- app/main.py
+++ app/main.py
@@ -3,11 +3,17 @@ from uuid import uuid4
 import boto3
 import json
 import logging
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import uuid
 
 
 
 app = FastAPI()
 transactions = {}
+templates = Jinja2Templates(directory="templates")
+
 
 @app.post("/pay")
 def make_payment(amount: float, method: str = "card"):
@@ -50,3 +56,14 @@ def pay(amount: float, method: str):
     transactions[txn_id] = {"amount": amount, "method": method, "status": "success"}
     logger.info(f"Payment processed | txn_id={txn_id} | amount={amount} | method={method}")
     return {"transaction_id": txn_id, "status": "success"}
+
+
+@app.get("/payform", response_class=HTMLResponse)
+def get_form(request: Request):
+    return templates.TemplateResponse("payform.html", {"request": request})
+
+@app.post("/payform", response_class=HTMLResponse)
+def submit_form(request: Request, amount: float = Form(...), method: str = Form(...)):
+    txn_id = str(uuid.uuid4())
+    transactions[txn_id] = {"amount": amount, "method": method, "status": "success"}
+    return HTMLResponse(content=f"<h3>Payment Successful!</h3><p>Transaction ID: {txn_id}</p>", status_code=200)
